ca6688a6629728e23fc8a9dd0a8ee182bdb7a28a
ChuTrungKien <chutrungkien11997@gmail.com>
2025-03-26 18:11:44 +0700

    kien commit them homecontroller

---
+++ PhotoIDApp/PhotoIDApp/controller/HomeViewController.swift
@@ -0,0 +1,15 @@
+//
+//  HomeViewController.swift
+//  PhotoIDApp
+//
+//  Created by QRVN on 26/3/25.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
